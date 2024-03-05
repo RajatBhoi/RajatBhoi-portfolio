d7fd211a225e61425a1462f610e2ff6b95645a93
RajatBhoi <134584727+RajatBhoi@users.noreply.github.com>
2024-03-05 16:16:07 +0000

    Testing

--- script/main.js
+++ script/main.js
@@ -1,1 +1,21 @@
-console.log("Rajat");
+//console.log("Rajat");
+const marvel_heros = ["Thor","Deadpool","Hulk"]
+const dc_heros = ["Flash","Batman","Doom"]
+
+//marvel_heros.push(dc_heros)
+
+//console.log(marvel_heros);
+//const all_heros = marvel_heros.concat(dc_heros)
+//console.log(all_heros);
+
+const allnewheros = [...marvel_heros,...dc_heros]
+//console.log(allnewheros);
+
+const another_array = [1,2,3,[4,5,6],7,[8,9,[3,6]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Rajat"));
+console.log(Array.from("Rajat"));
